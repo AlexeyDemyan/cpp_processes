2d339a23356d7934490790136a56d608e93f6637
Alexey <deal110988@him-mail.ch>
2025-03-31 16:45:08 +0200

    added creating file, writing to it and reading from it

--- create-file.cpp
+++ create-file.cpp
@@ -42,4 +42,72 @@ int main(int argc, char **argv)
     {
         printf("\n File created! \n");
     }
+
+    // WRITING TO FILE:
+
+    std::string strBuff = "Random text to place in file";
+    // I think I would prefer to separate this into 2 lines:
+    DWORD bytesToWrite = strBuff.size(), bytesWritten;
+
+    // writing buffer to file
+    BOOL bWrite = WriteFile(hFile, strBuff.c_str(), bytesToWrite, &bytesWritten, NULL);
+
+    if (bWrite)
+    {
+        printf("\n Buffer written to file \n");
+    }
+    else if (bytesToWrite != bytesWritten)
+    {
+        printf("Bytes written != bytes to write");
+        printf("\nSuccessfully wrote %d bytes to %s", bytesWritten, argv[1]);
+        CloseHandle(hFile);
+        return 1;
+    }
+    else
+    {
+        switch (GetLastError())
+        {
+        default:
+            printf("Error: WRITEFILE: %d\n", GetLastError());
+            CloseHandle(hFile);
+            return 1;
+        }
+    }
+    CloseHandle(hFile);
+
+    // opening handle to the already created file to read it:
+    hFile = CreateFileA(
+        argv[1],
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING, // this is to open already existing file with name of argv[1]
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+
+    DWORD nToRead = 15; // will read 15 bytes
+    char chBuff[16]; // mmm not sure if it's necessary to specify the size of this buffer
+    DWORD nRead;
+
+    // READING file:
+    BOOL bRead = ReadFile(hFile, &chBuff, nToRead, &nRead, NULL);
+    chBuff[15] = '\0'; // hmmm, terminating the string
+
+    if(bRead) {
+        printf("\n DATA read: %s\n", chBuff);
+    } else if (nToRead != nRead) {
+        printf("Number of bytes to read != number of bytes read\n");
+    } else { 
+        switch (GetLastError())
+        {
+        case ERROR_NOACCESS:
+            printf("\n ERROR: No access to read file %s \n", argv[1]);
+            break;
+        default:
+            printf("\n Error: READFILE %d \n", GetLastError());
+        } CloseHandle(hFile);
+        return 1;
+    }
+    CloseHandle(hFile);
+    return 0;
 }
---
+++ var-init-test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string someString = "Text", newText;
+    newText = "somsoms";
+    std::cout << "\n value of someString: " << someString << '\n';
+    std::cout << "\n value of newText: " << newText << '\n';
+}

053b9e4f5376e8456c39b0caa9797e657cbc3fb1
Alexey <deal110988@him-mail.ch>
2025-04-18 15:36:01 +0200

    managing to get process name and write it to the buffer

--- processes-from-ttrl.cpp
+++ processes-from-ttrl.cpp
@@ -5,10 +5,34 @@
 #include <iostream>
 #include <processthreadsapi.h>
 
-// void GetProcessNameById(DWORD pId)
-// {
+void GetProcessNameById(DWORD pId)
+{
 
-// }
+    // opening process and getting handle for it
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+                                      PROCESS_VM_READ,
+                                  FALSE, pId);
+
+    TCHAR procName[MAX_PATH] = _T("<Unknown>");
+
+    // checking if process is opened
+    if (hProcess != NULL)
+    {
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        // enumerating process modules
+        if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded))
+        {
+            // getting the base module name, which is actually the name of the process
+            // and then this name we're writing to a buffer
+            DWORD baseName = GetModuleBaseName(hProcess, hMod, procName, sizeof(procName) / sizeof(TCHAR));
+            std::cout << "Here's the process name from the buffer: " << procName << '\n';
+        }
+    }
+
+    CloseHandle(hProcess);
+}
 
 int ListProcesses()
 {
@@ -31,7 +55,8 @@ int ListProcesses()
     for (int i = 0; i < cbProcesses; i++)
     {
         // For the time being, just printing the process ID here instead of process name
-        std::cout << "Id of procces: " << processes[i] << "\n";
+        // std::cout << "Id of procces: " << processes[i] << "\n";
+        GetProcessNameById(processes[i]);
     }
     return 0;
 }

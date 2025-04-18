b7845ff8fb3d9d20f88a076c7203928927866b28
Alexey <deal110988@him-mail.ch>
2025-04-18 16:11:06 +0200

    appear to have found a way to match my process name

---
+++ hck_two.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+#include <iostream>
+#include <processthreadsapi.h>
+
+void lookForMyProcess(TCHAR *processName, std::string mySearchedProcessName)
+{   
+    
+    // std::cout << "Here's first character in processName TCHAR:" << processName[0] << "\n";
+    TCHAR toFind[] = _T("chrome.exe");
+    if(_tcscmp(processName, toFind) == 0) {
+        std::cout << "Found match!! : " << processName << "\n";
+    }
+
+    // if (*processName == mySearchedProcessName) {
+    //     std::cout << "Found match!! : " << mySearchedProcessName << "\n";
+    // }
+    // std::cout << "Reading process name in my function:" << processName << "\n";
+}
+
+void GetProcessNameById(DWORD pId)
+{
+
+    // opening process and getting handle for it
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+                                      PROCESS_VM_READ,
+                                  FALSE, pId);
+
+    TCHAR procName[MAX_PATH] = _T("<Unknown>");
+
+    // checking if process is opened
+    if (hProcess != NULL)
+    {
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        // enumerating process modules
+        if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded))
+        {
+            // getting the base module name, which is actually the name of the process
+            // and then this name we're writing to a buffer
+            DWORD baseName = GetModuleBaseName(hProcess, hMod, procName, sizeof(procName) / sizeof(TCHAR));
+            // std::cout << "Here's the process name from the buffer: " << procName << '\n';
+            lookForMyProcess(procName, "chrome.exe");
+        }
+    }
+
+    CloseHandle(hProcess);
+}
+
+int ListProcesses()
+{
+    DWORD processes[1024], cbNeeded, cbProcesses;
+
+    // Enumerating all processes
+    BOOL procBool = EnumProcesses(processes, sizeof(processes), &cbNeeded);
+
+    if (!procBool)
+    {
+        std::cout << "Unable to fetch processes\n";
+        return 1;
+    }
+
+    // This is basically the amount of currently running processes
+    cbProcesses = cbNeeded / sizeof(DWORD);
+
+    std::cout << "Value of cbProcesses: " << cbProcesses << "\n";
+
+    for (int i = 0; i < cbProcesses; i++)
+    {
+        // For the time being, just printing the process ID here instead of process name
+        // std::cout << "Id of procces: " << processes[i] << "\n";
+        GetProcessNameById(processes[i]);
+    }
+    return 0;
+}
+
+int main()
+{
+    ListProcesses();
+}

fb753e213b3850e9dd2072d0b3294df7ef4f8bc7
Alexey <deal110988@him-mail.ch>
2025-04-20 16:32:54 +0200

    added POST request implementation. For the time being, sending POST request from Postman so as to avoid implementing a frontend, since this requires CORS to be set up

--- pipe-server.js
+++ pipe-server.js
@@ -1,10 +1,39 @@
 const net = require('net');
+const http = require('http');
 const pipeName = '\\\\.\\pipe\\TestPipe';
 
 const client = net.createConnection(pipeName, () => {
     console.log('[+] Connected to pipe server');
 
     client.write('Hello from Web UI!');
+});
+
+const server = http.createServer((req, res) => {
+    // Routing
+    if (req.url === '/') {
+        if (req.method === 'POST') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Received POST request!');
+            client.write("Forwarding POST request");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Basic server running!');
+        }
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('About page');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
+
+
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
 
 client.on('data', (data) => {
